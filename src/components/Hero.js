0bd6c96ef494d76493bd2ad2b062c377868e9b38
Sadeka <sadekaahmed53@gmail.com>
2022-10-21 16:54:12 +0600

    Setup project

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { HiOutlineChevronDown } from 'react-icons/hi';
+import { hero } from '../data';
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
