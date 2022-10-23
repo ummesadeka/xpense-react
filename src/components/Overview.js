c93707e8e407ba9366ad335e58876ea0438fa240
Sadeka <sadekaahmed53@gmail.com>
2022-10-23 20:33:36 +0600

    Overview

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { overview } from '../data';
 const Overview = () => {
-  // destructure overview
-  return <div>Overview</div>;
+  // destructure overview data
+  const { productImg } = overview;
+  return (
+    <section className=" lg:min-h-[712px] bg-overview bg-cover bg bg-left-top pt-[30px] lg:pt-[87px]">
+      <div className="container mx-auto flex justify-end overflow-hidden">
+        <img src={productImg} alt="" data-aos="fade-up" daa-aos-offset="300" />
+      </div>
+    </section>
+  );
 };
 
 export default Overview;
