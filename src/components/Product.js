cfc533990d9c4cc71589286a3941df65f02a9b67
Sadeka <sadekaahmed53@gmail.com>
2022-10-24 20:49:24 +0600

    Product Card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,36 @@
 import React from 'react';
-
+import { product } from '../data';
+import Cards from './Cards';
 const Product = () => {
-  return <div>Products</div>;
+  // destructure product data
+  const { title, subtitle } = product;
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        {/* title & subtitle */}
+        <div className="flex flex-col items-center lg:flex-row mb-10 lg:mb-20">
+          <h2
+            className="section-title"
+            data-aos="fade-up"
+            data-aos-offset="400"
+            data-aos-delay="300"
+          >
+            {title}
+          </h2>
+          <p
+            className="lead lg:max-w-[350px]"
+            data-aos="fade-up"
+            data-aos-offset="400"
+            data-aos-delay="400"
+          >
+            {subtitle}
+          </p>
+        </div>
+        {/* cards */}
+        <Cards />
+      </div>
+    </section>
+  );
 };
 
 export default Product;
