28d52051ce6313de22ea58913438265153ec4c0d
Sadeka <sadekaahmed53@gmail.com>
2022-10-26 00:49:49 +0600

    Testimonial

--- src/components/ClientSlider.js
+++ src/components/ClientSlider.js
@@ -1,7 +1,60 @@
 import React from 'react';
+// swiper component
+import { Swiper, SwiperSlide } from 'swiper/react';
+// swiper styles
+import 'swiper/css';
 
-const ClientSlider = () => {
-  return <div>ClientSlider</div>;
+const ClientSlider = ({ clients }) => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      grabCursor={true}
+      loop={true}
+      breakpoints={{
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+        1170: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+    >
+      {clients.map((client, index) => {
+        // destructure client
+        const { message, image, name, position, borderColor } = client;
+        return (
+          <SwiperSlide
+            key={index}
+            style={{ borderColor: borderColor }}
+            className="border-t-[10px] rounded-t-[12px]"
+          >
+            {/* card */}
+            <div className="w-full mx-auto lg:max-w-[300px] xl:mx-w-[350px] h-[250px] rounded-[12px] border bottom-grey py-6 px-[30px]">
+              {/* person message */}
+              <div className="mb-[30px]">{message}</div>
+              {/* person name, img & position */}
+              <div className="flex gap-x-[20px]">
+                <img src={image} alt="" />
+                <div className="font-bold">{name}</div>
+                <div className="text-light">{position}</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default ClientSlider;
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { testimonials } from '../data';
+import ClientSlider from './ClientSlider';
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  // destructure testimonial data
+  const { title, clients } = testimonials;
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        {/* title */}
+        <h2
+          className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
+          {title}
+        </h2>
+        {/* slider */}
+        <div data-aos="fade-up" data-aos-delay="400">
+          <ClientSlider clients={clients} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
